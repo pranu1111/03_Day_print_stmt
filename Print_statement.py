ced43ef61abb58e851d35f247c5d175fde9bf0c9
pranu1111 <107885176+pranu1111@users.noreply.github.com>
2022-06-23 01:36:29 +0530

    Add files via upload

---
+++ Print_statement.py
@@ -0,0 +1,21 @@
+print('hello world')    #print stmt with single quote
+print("hello world")    # print stmt with double quote
+print('hello \
+world')                  # print stmt with backward slash wirh single quote 
+print("hello \
+           world")         # print stmt with backward slash wirh double quote
+print('''hello world''')    # print stmt with 3 times single quote (xerox copy)
+print("""hello world""")    # print stmt with 3 times double quote (xerox copy)
+print('''hello 
+         world''')            # xerox copy
+print("""hello 
+      
+      
+    world""")
+
+print(""" *
+ **
+ ***
+ ****
+ *****
+ ******""")
